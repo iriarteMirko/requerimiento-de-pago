9e92d74a1c968531bb5e8b8a5222a33251b34a77
Mirko Iriarte <20184037@aloe.ulima.edu.pe>
2024-05-13 16:40:20 -0500

    Update numeros.py

--- src/models/numeros.py
+++ src/models/numeros.py
@@ -24,6 +24,9 @@ def convertir_grupo_a_texto(num):
         if num > 100:
             texto += centenas[num // 100]
             num %= 100
+        elif num == 100:
+            texto += "cien"
+            num = 0
         
         if num >= 30:
             texto += " " + decenas[num // 10]
@@ -46,6 +49,7 @@ def convertir_grupo_a_texto(num):
                 texto += " un"
             elif num == 0:
                 pass
+    
     return texto.strip()
 
 def numero_entero_a_texto(num):
@@ -55,6 +59,7 @@ def numero_entero_a_texto(num):
         return "cien"
     elif num == 1000:
         return "mil"
+    
     grupos = []
     while num > 0:
         grupos.append(num % 1000)
@@ -72,19 +77,23 @@ def numero_entero_a_texto(num):
             textos[1] = textos[1].replace("uno mil", "un mil")
     if len(textos) > 2:
         textos[2] = "un millón" if textos[2] == "uno" else textos[2] + " millones"
+    
     return " ".join(textos[::-1]).strip().replace("  ", " ")
 
 def formato_miles(num):
     num_str = str(num)
     if len(num_str) <= 3:
         return num_str
+    
     miles = num_str[:-3]
     cientos = num_str[-3:]
+    
     miles_con_comas = ""
     for i, digito in enumerate(miles[::-1]):
         miles_con_comas += digito
         if (i + 1) % 3 == 0 and (i + 1) != len(miles):
             miles_con_comas += ","
+    
     return miles_con_comas[::-1] + cientos
 
 def formato_numero(num):
